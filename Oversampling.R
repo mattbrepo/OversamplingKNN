703709fc0a80dbc07bf43e4ce374908c2ec4d51d
Matteo <matteo.bertola77@gmail.com>
2022-04-23 10:44:49 +0200

    Initial commit

---
+++ Oversampling.R
@@ -0,0 +1,62 @@
+library(caret)
+library(class)
+library(data.table)
+
+#
+# Experiment on oversampling with KNN model for binary classification
+#
+
+# create data with one item clearly in the wrong place
+countA = 12
+countB = 4
+countBWorng = 2
+df <- data.frame (X1 = c(rnorm(countA + countBWorng, mean=1, sd=0.1), rnorm(countB - countBWorng, mean=2, sd=0.1)),
+                  X2 = c(rnorm(countA + countBWorng, mean=1, sd=0.1), rnorm(countB - countBWorng, mean=2, sd=0.1)),
+                  Label = c(rep("A", countA), rep("B", countB))
+)
+df$LabelInt = 1
+df[df$Label == "B", "LabelInt"] = -1
+
+# plot the data
+plot(x = df$X1, y = df$X2, xlab = "X1", ylab = "X2", cex = 2, col = "black", pch = ifelse(df$Label == "A", 1, 4), main="Class A: circle, Class B: cross")
+
+# KNN classification
+myMtxX = as.matrix(df[c("X1", "X2")])
+myMtxY = as.matrix(df["LabelInt"])
+yPred <- class::knn(myMtxX, myMtxX, cl=myMtxY, k=3) 
+
+# plot the data (with color prediction)
+plot(x = df$X1, y = df$X2, xlab = "X1", ylab = "X2", cex = 3, col = ifelse(yPred == myMtxY, "black", "red"), pch = ifelse(df$Label == "A", 21, 4), main="Class A: circle, Class B: cross, Red: wrong prediction")
+
+# confusion matrix
+cm = caret::confusionMatrix(as.factor(yPred), as.factor(df$LabelInt), positive="1") # NB: in caret the second parameter is the reference!
+t(cm$table)
+
+# random oversampling
+countSampB = countA - countB
+dfOS = data.frame(df)
+dfOS$CountLabel = "" # add a string column containing the number of duplicates
+
+dfB = dfOS[dfOS$Label == "B", ]
+sampleBs = sample(nrow(dfB), countSampB, replace = TRUE)
+
+tableBs = as.data.frame(table(sampleBs))
+dfB[sampleBs, "CountLabel"] = tableBs[sampleBs, "Freq"] # number of duplicates
+
+dfBOS = dfB[sampleBs, ]
+dfOS = rbind(dfOS, dfBOS) # df OverSampled
+
+# KNN classification of oversampled data
+myMtxX2 = as.matrix(dfOS[c("X1", "X2")])
+myMtxY2 = as.matrix(dfOS["LabelInt"])
+yPredOS <- class::knn(myMtxX2, myMtxX2, cl=myMtxY2, k=3)
+
+# plot the data (including the number of repetition)
+plot(x = dfOS$X1, y = dfOS$X2, xlab = "X1", ylab = "X2", cex = 2, col = ifelse(yPredOS == myMtxY2, "black", "red"), pch = ifelse(dfOS$Label == "A", 21, 4), main="Red: wrong prediction, Num: count of oversampled duplicates")
+text(dfOS$X1, dfOS$X2, labels = dfOS$CountLabel, pos = 4)
+
+# confusion matrix of oversampled data
+cmOS = caret::confusionMatrix(as.factor(yPredOS), as.factor(dfOS$LabelInt), positive="1") # NB: in caret the second parameter is the reference!
+t(cmOS$table)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # OversamplingKNN
-Experiments on oversampling with KNN classification model
+Experiments on [oversampling](https://en.wikipedia.org/wiki/Oversampling_and_undersampling_in_data_analysis) with [KNN classification model](https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm)
+
+**Language: R**
+
+**Start: 2022**
+
+## Why
+I had an unbalanced dataset that I needed to use to build a binary classification model. Since it had unbalanced classes (i.e., one class was way more present than the other), I tried to use a random oversampling technique to improve the training of the model. The problem forced me to put together this R experiment to study the effect of oversampling on KNN models.
+
+## Example
+
+I created an unbalanced data frame with 16 points associated with two classes:
+
+![Example](/images/plot1.jpg)
+
+Understandably, the KNN model trained on such data (with k = 3) classifies the two points (class B, cross symbol) that are on the left side of the following image:
+
+![Example](/images/plot2.jpg)
+
+Here is the confusion matrix:
+
+Reference   | Prediction A | Prediction B
+------------|--------------|--------------
+Reference A |      12      |      0
+Reference B |       2      |      2
+
+I applied random oversampling and retrained the KNN model:
+
+![Example](/images/plot3.jpg)
+
+The oversampling did improve the prediction of the underrepresented class (class B, cross symbol), but had a negative effect on the prediction of the class A points. This is understood, since the creation of random class B points affects the prediction of those class A points that are close to such class B points.
